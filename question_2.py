f19c4a0c28a2a086585921d7bf5169fb5c5649cd
tcarlisle10 <tylercarlisle10@yahoo.com>
2024-01-02 20:39:33 -0600

    updated

--- question_2.py
+++ question_2.py
@@ -1,1 +1,11 @@
 def first_odds():
+    """Count odd numbers to 100"""
+    current_number = 0 
+    while current_number < 100:
+        current_number += 1
+        if current_number % 2 == 0:
+            continue
+
+        print(current_number)
+    
+first_odds()
