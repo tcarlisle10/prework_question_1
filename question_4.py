43c16a6cb3421892746c7351ca3f2c14fe65fa00
tcarlisle10 <tylercarlisle10@yahoo.com>
2024-01-05 11:03:01 -0600

    update

--- question_4.py
+++ question_4.py
@@ -1,1 +1,12 @@
-def is_leap_year(a_year):
+def is_leap_year(year):
+
+    if year % 4 == 0:
+
+        print("this is a leap year!")
+    
+    elif year % 400 == 0 or year % 100 != 0:
+
+        print("this is not a leap year!")
+
+
+is_leap_year(2022)
